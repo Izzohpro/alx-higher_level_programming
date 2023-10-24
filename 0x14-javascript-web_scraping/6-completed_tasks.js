37860abd4791daec54ed530a5c7488fa3007f504
Izzohpro <okparafrankline@gmail.com>
2023-10-24 10:45:00 -0700

    Completed

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,12 +1,25 @@
 #!/usr/bin/node
 
 const request = require('request');
-const fs = require('fs');
 
-request(process.argv[2], function (_err, _res, body) {
-  fs.writeFile(process.argv[3], body, 'utf8', function (err) {
-    if (err) {
-      console.log(err);
+request(process.argv[2], function (err, _res, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    const completedTasksByUsers = {};
+    body = JSON.parse(body);
+
+    for (let i = 0; i < body.length; ++i) {
+      const userId = body[i].userId;
+      const completed = body[i].completed;
+
+      if (completed && !completedTasksByUsers[userId]) {
+        completedTasksByUsers[userId] = 0;
+      }
+
+      if (completed) ++completedTasksByUsers[userId];
     }
-  });
+
+    console.log(completedTasksByUsers);
+  }
 });
